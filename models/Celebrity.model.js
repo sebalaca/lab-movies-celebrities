ee3640aa817f6bc9659402fb89322e43d38f64e4
Sebastian Laca <seba.laca@gmail.com>
2022-06-28 14:56:42 +0200

    Iteracion 2 Complete

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
-//  Add your code here
+// Iteration #2
+const mongoose = require("mongoose");
+
+const celebritySchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  occupation: {
+    type: String
+  },
+  catchPhrase: {
+    type: String
+  }
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+module.exports = Celebrity;
